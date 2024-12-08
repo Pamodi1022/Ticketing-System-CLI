64e0562623bacc7876600c81bb13246ace3426a7
Pamodi1022 <pamodipansiluni1022@gmail.com>
2024-12-08 20:33:28 +0530

    feat: add Customer class with ticket retrieval functionality
    - Implemented `Customer` class implementing `Runnable` for multithreaded ticket retrieval.
    - Included attributes for `ticketPool`, `customerId`, and `retrievalRate`.
    - Added logic to retrieve tickets from the pool at a specified rate.
    - Handled thread interruption gracefully.

---
+++ src/CLI/Customer.java
@@ -0,0 +1,29 @@
+package CLI;
+
+public class Customer implements Runnable {
+    private final TicketPool ticketPool;
+    private final int customerId;
+    private final int retrievalRate;
+    private boolean running = true;
+
+    public Customer(TicketPool ticketPool, int customerId, int retrievalRate) {
+        this.ticketPool = ticketPool;
+        this.customerId = customerId;
+        this.retrievalRate = retrievalRate;
+    }
+
+    @Override
+    public void run() {
+        while (running) {
+            if (ticketPool.isSimulationActive()){
+                break;
+            }
+            ticketPool.removeTicket(customerId);
+            try{
+                Thread.sleep(retrievalRate * 1000L);
+            }catch(InterruptedException e){
+                System.out.println("Customer: " + customerId + " is interrupted");
+            }
+        }
+    }
+}
